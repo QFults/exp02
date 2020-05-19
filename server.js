4526a983b0d9e15b0ea1e1fa3b7231844299851a
Quinton Fults <coding.banjo@gmail.com>
2020-05-19 11:38:03 -0700

    code

--- server.js
+++ server.js
@@ -1,30 +1,8 @@
 const express = require('express')
-
+const { join } = require('path')
 const app = express()
 
-// app.get('/questions/:id/:title', (req, res) => {
-//   console.log(req.params)
-//   res.json(req.params)
-// })
+app.use(express.static(join(__dirname, 'public')))
 
-app.get('/:op/:a/:b', (req, res) => {
-  let a = parseInt(req.params.a)
-  let b = parseInt(req.params.b)
-
-  switch (req.params.op) {
-    case 'add':
-      res.send(`${a + b}`)
-      break
-    case 'subtract':
-      res.send(`${a - b}`)
-      break
-    case 'multiply':
-      res.send(`${a * b}`)
-      break
-    case 'divide':
-      res.send(`${a / b}`)
-      break
-  }
-})
 
 app.listen(3000, () => console.log('http://localhost:3000'))
