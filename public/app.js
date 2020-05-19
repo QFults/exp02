12fe4f1217861b925fad0f7f250c6e8eb5c8df78
Quinton Fults <coding.banjo@gmail.com>
2020-05-19 13:38:19 -0700

    code

--- public/app.js
+++ public/app.js
@@ -1,1 +1,16 @@
-console.log('ping')
+
+document.getElementById('increment').addEventListener('click', event => {
+  
+  axios.get('/increment')
+    .then(response => {
+      document.getElementById('count').textContent = response.data.count
+    })
+    .catch(err => console.error(err))
+
+})
+
+axios.get('/count')
+  .then(response => {
+    document.getElementById('count').textContent = response.data.count
+  })
+  .catch(err => console.error(err))
--- server.js
+++ server.js
@@ -4,5 +4,21 @@ const app = express()
 
 app.use(express.static(join(__dirname, 'public')))
 
+let count = 0
+
+app.get('/increment', (req, res) => {
+
+  count++
+
+  res.json({
+    count
+  })
+})
+
+app.get('/count', (req, res) => {
+  res.json({
+    count
+  })
+})
 
 app.listen(3000, () => console.log('http://localhost:3000'))
